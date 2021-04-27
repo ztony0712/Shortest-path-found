3ee9fcbd30842448f572b00a2401e3b9ddbf6bfa
ztony0712 <ztony0712@outlook.com>
2021-04-27 16:09:26 +0800

    load 2.0

--- file_management.c
+++ file_management.c
@@ -6,12 +6,14 @@
 extern Node *graph[4000];
 
 int load_map(FILE *file) {
+    memset(graph, 0, sizeof(Node));
     char line[200];
     int i = 0;
     if (file == NULL) 
         return 1;   
 
     rewind(file);
+    // read all nodes in graph array
     while(fgets(line, 200, file) != NULL) {
 
         if (strstr(line, "lon")) {
@@ -22,26 +24,48 @@ int load_map(FILE *file) {
         }
     }
     rewind(file);
+    // read all links and connect them to relevant nodes
     while(fgets(line, 200, file) != NULL) {
         if (strstr(line, "link")) {
-            long int source_id = 0, container1 = 0, container2 = 0;
-            Neighbor *new_neighbor = (Neighbor*) malloc(sizeof(Neighbor));
-            memset(new_neighbor, 0, sizeof(Neighbor));
-            sscanf(line, "%*[^=]=%ld %*[^=]=%ld %*[^=]=%ld %*[^=]=%ld %*[^=]=%lf", &container1, &source_id, &new_neighbor->id, &container2, &new_neighbor->dis);
+            long int container1 = 0, container2 = 0;
+            double dis;
+            Neighbor *new_neighbor1 = (Neighbor*) malloc(sizeof(Neighbor));
+            memset(new_neighbor1, 0, sizeof(Neighbor));
+            Neighbor *new_neighbor2 = (Neighbor*) malloc(sizeof(Neighbor));
+            memset(new_neighbor2, 0, sizeof(Neighbor));
+            
+            sscanf(line, "%*[^=]=%ld %*[^=]=%ld %*[^=]=%ld %*[^=]=%ld %*[^=]=%lf", &container1, &new_neighbor1->id, &new_neighbor2->id, &container2, &dis);
             for (int i = 0; graph[i] != NULL; ++i) {
                 
-                if (graph[i]->id == source_id) {
-                    // new_neighbor->next = graph[i]->near;
+                // find first node id in graph array
+                if (graph[i]->id == new_neighbor1->id) {
+                    // give distance to both new_neighbor nodes
+                    new_neighbor1->dis = new_neighbor2->dis = dis;
+                    // link second node to first node
                     if (graph[i]->near == NULL) {
-                        graph[i]->near = new_neighbor;
+                        graph[i]->near = new_neighbor2;
                     } else {
                         Neighbor *current = graph[i]->near;
                         for (; current->next != NULL; current = current->next);
-                        current->next = new_neighbor;
+                        current->next = new_neighbor2;
 
+                    }
+                    // go through the array again to find the second node
+                    // and link the first node to second node
+                    for (int j = 0; graph[j]; ++j) {
+                        if (graph[j]->id == new_neighbor2->id) {
+                            if (graph[j]->near == NULL) {
+                                graph[j]->near = new_neighbor1;
+                            } else {
+                                Neighbor *current = graph[j]->near;
+                                for (; current->next != NULL; current = current->next);
+                                current->next = new_neighbor1;
+                            }
+                        }
                     }
                     
                 }
+
             }
         }
     }
@@ -55,5 +79,6 @@ int load_map(FILE *file) {
 }
 
 int store_map(FILE *file) {
- return 1;
+    
+    return 1;
 }
--- main.c
+++ main.c
@@ -7,13 +7,17 @@ Node *graph[4000];
 int main(int argc, char const *argv[])
 {
     FILE *file;
-    file = fopen("Final_Map.map", "r");
+    file = fopen("testMap.map", "r");
     if (load_map(file) == 1)
         puts("No map file!");
     else
         puts("Map loaded.");
 
-
+    file = fopen("nodes.txt", "w");
+    if (load_map(file) == 1)
+        puts("No map file!");
+    else
+        puts("Map loaded.");
 
     return 0;
 }
